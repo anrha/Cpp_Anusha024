ed0eadebd6ba429b927d34dd26f58b5b43ecf8bb
Anusha RH <anushahadimani64@gmail.com>
2026-09-13 15:48:32 +0530

    Add Reverse words.cpp

---
+++ Reverse words.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ss stream>
+#include <string>
+using namespace std;
+int main() {
+  string s, word;
+    cout<<"Enter a sentence: ";
+    getline(cin, s);
+    stringstream ss(s);
+    string words[100];
+    int count = 0;
+    while (ss >> word) {
+        words[count++] = word;
+    count++;
+    }
+    cout<<"Reverse sentence: ";
+    for (int i = count - 1; i >= 0; i--) {
+        cout<<words[i]<<" ";
+    }
+    return 0;
+}
